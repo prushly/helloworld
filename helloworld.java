1cca0b86e707d2ddf9f2262eb9881864bd4e1441
prushly <purushothptm@gmail.com>
2017-07-12 11:21:11 +0530

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,19 @@
+
+public class Ideone {
+
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int a=1;
+		
+	while(a<=5){
+		System.out.println("Hello world");
+		
+	
+		
+		a++;
+	
+		
+	}}
+
+}
